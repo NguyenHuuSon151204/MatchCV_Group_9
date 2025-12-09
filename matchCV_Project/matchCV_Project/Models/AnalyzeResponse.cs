7eefd35d4fdca058e9005c94414d7d6108ee8316
kunzxn <lequochung.17g@gmail.com>
2025-12-09 09:27:38 +0700

    Delete model not use for project

--- matchCV_Project/matchCV_Project/Models/AnalyzeResponse.cs
+++ matchCV_Project/matchCV_Project/Models/AnalyzeResponse.cs
@@ -1,7 +1,8 @@
 ﻿namespace matchCV_Project.Models;
 
 public class AnalyzeResponse
-{
+{ 
+     
     public double Score { get; set; }
     public List<string> MatchedSkills { get; set; } = new();
     public List<string> MissingSkills { get; set; } = new();
